eba6f20263d847eec87e5904b8204f261da52f30
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-10-01 04:31:31 +0200

    Update management.sh

--- setup/management.sh
+++ setup/management.sh
@@ -19,7 +19,7 @@ apt_install duplicity python3-pip virtualenv certbot rsync
 # b2sdk is used for backblaze backups.
 # boto3 is used for amazon aws backups.
 # Both are installed outside the pipenv, so they can be used by duplicity
-hide_output pip3 install --upgrade b2sdk boto3
+hide_output pip install --upgrade b2sdk boto3
 
 # Create a virtualenv for the installation of Python 3 packages
 # used by the management daemon.
